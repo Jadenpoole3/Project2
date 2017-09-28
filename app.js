0ac79ddec9feb0fb07e4425d53e9b6add7bdab39
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-28 11:39:56 -0400

    Installed mongoose and made a heroku uri installed dotenv and successfully hid the env and required mongoose

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
