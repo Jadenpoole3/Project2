0b12f42c80e103e05730c6cd67c8eb96a98ee11c
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-28 23:00:05 -0400

    made a index route in questions

--- routes/questionsController.js
+++ routes/questionsController.js
@@ -1,1 +1,29 @@
 const express = require('express')
+
+const router = express.Router({ mergeParams: true })
+
+const Schema = require("../db/schema.js");
+const CompanyModel = Schema.CompanyModel;
+
+//index route
+route.get('/', (request,response) => {
+    // get the id from the params 
+    const companyId = request.params.companyId
+
+    //Use the company model to find the id
+    CompanyModel.findById(companyId)
+    .then((company) => {
+        //Render the company and use handlebars
+        response.render('questions/index', {
+            company: company
+        })
+  
+
+    })
+    // catch the error 
+    .catch((error) => {
+        console.log(error)
+    })
+
+    
+})
