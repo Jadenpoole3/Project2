85eb0f9acfcf23eda8f00f69a4906eab880ffa56
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-28 23:36:48 -0400

    add a create route

--- routes/questionController.js
+++ routes/questionController.js
@@ -28,4 +28,32 @@ router.get('/', (request,response) => {
     
 })
 
+// CREATE route
+router.post('/', (request, response) => {
+    
+        // Get the company ID from the param
+        const companyId = request.params.companyId
+    
+        // Get the new question from the request body
+        const newQuestion = request.body
+    
+        // USE the CompanyModel to find the company by ID
+        CompanyModel.findById(companyId)
+            .then((company) => {
+                // THEN once you have found the company from the database
+                // PUSH the new snowboard object into the Company's 
+                // snowboard array            
+                company.questions.push(newQuestion)
+    
+                // SAVE the company and return the PROMISE
+                return company.save()
+            })
+            .then((company) => {
+                // THEN once the company has been saved, 
+                // REDIRECT to the Snowboards index for that company
+                response.redirect(`/companies/${companyId}/questions`)
+            })
+    
+    })
+
 module.exports = router
